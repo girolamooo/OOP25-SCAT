1393ff570ac96ccc60f23ed9fe5efff8ccb8c6a2
Girolamo Ronzoni <girolamo.ronzoni@studio.unibo.it>
2026-01-03 16:19:52 +0100

    Small changes on the project structure: - package names - gradle settings (project main class)

---
+++ src/main/java/it/unibo/scat/App.java
@@ -0,0 +1,23 @@
+package it.unibo.scat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Main application class for OOP25-SCAT.
+ */
+public final class App {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    private App() { }
+
+    /**
+     * Application entry point.
+     * 
+     * @param args command line arguments
+     */
+    public static void main(final String[] args) {
+        LOGGER.info("Running OOP25-SCAT...");
+    }
+}
---
+++ src/test/java/it/unibo/scat/AppTest.java
@@ -0,0 +1,15 @@
+package it.unibo.scat;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Basic test for App.
+ */
+class AppTest {
+    @Test
+    void testAppFlow() {
+        final String status = "OK";
+        assertNotNull(status);
+    }
+}
