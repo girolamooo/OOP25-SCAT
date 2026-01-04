23fe2ca920e4b9d1c595332d4ad5470cf6a6d9d9
Girolamo Ronzoni <girolamo.ronzoni@studio.unibo.it>
2026-01-04 22:40:53 +0100

    init ModelObserver

--- src/main/java/it/unibo/scat/model/api/ModelObservable.java
+++ src/main/java/it/unibo/scat/model/api/ModelObservable.java
@@ -1,8 +1,21 @@
 package it.unibo.scat.model.api;
 
+import java.util.List;
+
 /**
  * Read-only interface for the Model class, seen from the View.
  */
 public interface ModelObservable {
 
+    /**
+     * @return ...
+     * 
+     */
+    int getScore();
+
+    /**
+     * @return ...
+     * 
+     */
+    List<Record> getLeaderboard();
 }
