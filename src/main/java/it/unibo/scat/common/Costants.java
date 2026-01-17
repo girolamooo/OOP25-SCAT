55884fb4fe84d8f937ebc2a2d442bf8f3f88057e
Mario Lungu <mario.lungu@studio.unibo.it>
2026-01-17 22:19:19 +0100

    solved checkstyle problem

--- src/main/java/it/unibo/scat/common/Costants.java
+++ src/main/java/it/unibo/scat/common/Costants.java
@@ -1,11 +1,14 @@
 package it.unibo.scat.common;
 
+/**
+ * ...
+ */
 public final class Costants {
     public static final int WORLD_WIDTH = 59;
     public static final int WORLD_HEIGHT = 36;
 
     /**
-     * Private constructor to avoid initialization
+     * Private constructor to avoid initialization.
      */
     private Costants() {
     }
