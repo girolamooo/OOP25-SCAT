03d22d1b9731412ab2d9d5c47cf08d45e7d0a072
Leonardo Lioi <leonardo.lioi@studio.unibo.it>
2026-01-17 16:59:29 +0100

    Leaderboard: wrote initLeaderboard()

--- src/main/java/it/unibo/scat/model/leaderboard/Leaderboard.java
+++ src/main/java/it/unibo/scat/model/leaderboard/Leaderboard.java
@@ -1,9 +1,22 @@
 package it.unibo.scat.model.leaderboard;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+import it.unibo.scat.common.EntityType;
 import it.unibo.scat.common.GameRecord;
+import it.unibo.scat.model.game.entity.AbstractEntity;
+import it.unibo.scat.model.game.entity.Bunker;
+import it.unibo.scat.model.game.entity.Invader;
+import it.unibo.scat.model.game.entity.Player;
 
 /**
  * This class handles the leaderboard logic.
@@ -16,6 +29,7 @@ public class Leaderboard {
      */
     public Leaderboard() {
         this.games = new ArrayList<>();
+
     }
 
     /**
@@ -25,6 +39,41 @@ public class Leaderboard {
      */
     public void initLeaderboard(final String filename) {
 
+        final int idxName = 0;
+        final int idxScore = 1;
+        final int idxLevel = 2;
+        final int idxDate = 3;
+
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        Objects.requireNonNull(
+                                getClass().getClassLoader().getResourceAsStream(filename)),
+                        StandardCharsets.UTF_8))) {
+
+            String line;
+            String name;
+            int score;
+            int level;
+            LocalDate date;
+
+            line = reader.readLine();
+            while (line != null) {
+                final String[] field = line.trim().split(";");
+
+                name = String.valueOf(field[idxName]);
+                score = Integer.parseInt(field[idxScore]);
+                level = Integer.parseInt(field[idxLevel]);
+                date = LocalDate.parse(field[idxDate].trim(), DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+
+                games.add(new GameRecord(name, score, level, date));
+
+                line = reader.readLine();
+
+            }
+
+        } catch (final IOException e) {
+            throw new IllegalStateException("Cannot load records from file: " + filename + "Exception: ", e);
+        }
     }
 
     /**
