c8fa884c9d6c210432a184ae75729e4bee4d1852
Mario Lungu <mario.lungu@studio.unibo.it>
2026-01-17 21:07:46 +0100

    Created Costants class in common

---
+++ src/main/java/it/unibo/scat/common/Costants.java
@@ -0,0 +1,12 @@
+package it.unibo.scat.common;
+
+public final class Costants {
+    public static final int GRID_WIDTH = 59;
+    public static final int GRID_HEIGHT = 36;
+
+    /**
+     * Private constructor to avoid initialization
+     */
+    private Costants() {
+    }
+}
