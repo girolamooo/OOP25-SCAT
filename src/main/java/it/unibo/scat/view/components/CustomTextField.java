f9b17e6eeb594b5bd22191702671ebee461e4d1d
Mario Lungu <mario.lungu@studio.unibo.it>
2026-01-25 22:27:39 +0100

    CustomTextField created

---
+++ src/main/java/it/unibo/scat/view/components/CustomTextField.java
@@ -0,0 +1,32 @@
+package it.unibo.scat.view.components;
+
+import javax.swing.JTextField;
+
+/**
+ * ...
+ */
+public final class CustomTextField extends JTextField {
+
+    public CustomTextField() {
+        // setFont(baseFont);
+
+        // FontMetrics fm = usernameField.getFontMetrics(baseFont);
+        // int maxWidth = fm.charWidth('W') * 15 + usernameField.getInsets().left +
+        // usernameField.getInsets().right;
+        // int maxHeight = fm.getHeight() + usernameField.getInsets().top +
+        // usernameField.getInsets().bottom;
+
+        // usernameField.setMaximumSize(new Dimension(maxWidth, maxHeight));
+
+        // usernameField.setDocument(new PlainDocument() {
+        // @Override
+        // public void insertString(int offs, String str, AttributeSet a)
+        // throws BadLocationException {
+        // if (getLength() + str.length() <= 15) {
+        // super.insertString(offs, str, a);
+        // }
+        // }
+        // });
+
+    }
+}
