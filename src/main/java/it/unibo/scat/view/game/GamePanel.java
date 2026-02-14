6afe4284cde9a22e3985c94af3fdc9785e18ad90
Mario Lungu <mario.lungu@studio.unibo.it>
2026-02-14 13:00:27 +0100

    GamePanel: changed MenuActionsInterface to ViewActionsInterface

--- src/main/java/it/unibo/scat/view/game/GamePanel.java
+++ src/main/java/it/unibo/scat/view/game/GamePanel.java
@@ -19,7 +19,7 @@ import javax.swing.SwingUtilities;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import it.unibo.scat.common.GameState;
 import it.unibo.scat.view.UIConstants;
-import it.unibo.scat.view.api.MenuActionsInterface;
+import it.unibo.scat.view.api.ViewActionsInterface;
 import it.unibo.scat.view.game.api.GamePanelInterface;
 import it.unibo.scat.view.game.canvas.Canvas;
 import it.unibo.scat.view.game.statusbar.StatusBar;
@@ -30,7 +30,7 @@ import it.unibo.scat.view.game.statusbar.StatusBar;
 @SuppressFBWarnings({ "SE_TRANSIENT_FIELD_NOT_RESTORED", "EI_EXPOSE_REP2" })
 public final class GamePanel extends JPanel implements GamePanelInterface {
     private static final long serialVersionUID = 1L;
-    private final transient MenuActionsInterface viewInterface;
+    private final transient ViewActionsInterface viewInterface;
     private transient List<BufferedImage> backgrounds;
 
     private Canvas canvas;
@@ -46,7 +46,7 @@ public final class GamePanel extends JPanel implements GamePanelInterface {
      * 
      * @param viewInterface the menu actions interface.
      */
-    public GamePanel(final MenuActionsInterface viewInterface) {
+    public GamePanel(final ViewActionsInterface viewInterface) {
         this.viewInterface = viewInterface;
 
         setLayout(new BorderLayout());
